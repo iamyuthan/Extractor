cf83956232f57f5442ac2e726386c26cb25dbdb8
Yuthan K <83505381+iamyuthan@users.noreply.github.com>
2023-05-03 20:56:25 +1000

    Add files via upload

---
+++ src/burp/persistence/InScopeTools.java
@@ -0,0 +1,24 @@
+package burp.persistence;
+
+public class InScopeTools {
+	public boolean allTools;
+	public boolean proxy;
+	public boolean scanner;
+	public boolean intruder;
+	public boolean repeater;
+	public boolean extender;
+
+	public InScopeTools(boolean allTools,
+						boolean proxy,
+						boolean scanner,
+						boolean intruder,
+						boolean repeater,
+						boolean extender) {
+		this.allTools = allTools;
+		this.proxy = proxy;
+		this.scanner = scanner;
+		this.intruder = intruder;
+		this.repeater = repeater;
+		this.extender = extender;
+	}
+}
